16b3eca86f2871ccc59e30d77d7dabf34ae38a26
lordking1234 <92648020+lordking1234@users.noreply.github.com>
2023-02-23 19:47:57 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+function increment(labelValue) { 
+  if (Math.abs(Number(labelValue)) >= 1.0e+9)
+return (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(2) + " B"
+    if (Math.abs(Number(labelValue)) >= 1.0e+6)
+return (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(2) + " M"
+    if (Math.abs(Number(labelValue)) >= 1.0e+3)
+return (Math.abs(Number(labelValue) / 1.0e+3).toFixed(2)) + " K"
+}
+
+
+fetch('https://api.flipsidecrypto.com/api/v2/queries/f4a144b5-8948-45bd-bb40-74b16050d0e7/data/latest').then(res=>res.json()).then(data=>{
+  const newData = data.reduce((t,i)=>{
+  return {...t,
+    trans:[...t.trans,i.TRANSACTIONS],
+    user:[...t.user,i.ADDRESS],
+    fee:[...t.fee,i.FEE],
+    gas:[...t.gas,i.GAS_PRICS]
+  }
+
+},{trans:[],user:[],fee:[],gas:[]})
+document.getElementById("first-box").innerHTML = increment(newData.trans)
+document.getElementById("second-box").innerHTML = increment(newData.user)
+document.getElementById("third-box").innerHTML = increment(newData.fee)
+document.getElementById("fourth-box").innerHTML = (Math.floor((newData.gas)*1000)/1000)
+})
 
 
 
@@ -5,66 +30,98 @@
 
 
 
+fetch('https://api.flipsidecrypto.com/api/v2/queries/77925504-e349-4ed6-a552-242cbafe4835/data/latest').then(res=>res.json()).then(data=>{
+  const newData = data.reduce((t,i)=>{
+  return {...t,
+    date:[...t.date,i.DATE],
+    tvl:[...t.tvl,i.TVL_USD]
+  }
+
+},{date:[],tvl:[],fee:[],gas:[]})
 
 
-var chartDom = document.getElementById('main');
-var myChart = echarts.init(chartDom);
-var option;
+chart2 = {
+  xAxis: {
+    type: 'category',
+    data: newData.date
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: newData.tvl,
+      type: 'line',
+      color :'#ff0000' ,
+      smooth: true
+    }
+  ]
+};
+echarts.init(document.getElementById('sixth-box')).setOption(chart2);
 
-option = {
+})
+
+
+
+fetch('https://api.flipsidecrypto.com/api/v2/queries/ed191a52-2fa3-49af-a3a2-3e770c85bd78/data/latest').then(res=>res.json()).then(data=>{
+  let selectedProps = data.reduce((acc, curr) => {
+    let { TVL_USD:value,CHAIN:name } = curr;
+    return [...acc, { value,name }];
+  }, []);
+
+  
+
+chart3 = {
   title: {
     text: '',
     subtext: '',
-    left: 'center' 
-
+    left: 'center'
   },
   tooltip: {
     trigger: 'item'
   },
   legend: {
     orient: 'vertical',
-    left: 'left' 
+    left: 'left'
   },
   series: [
     {
- 
       name: '',
-      type: 'pie',
-            color: [
-        '#37A2DA',
-        '#32C5E9',
-        '#67E0E3',
-        '#9FE6B8',
+      type: 'pie',      
+      label: {  show: true
+      },
+
+      color: [
+        '#fb7293',
+        '#ca6666',
+       '#d37f7f',
+       '#db9999',
+       '#e4b2b2',
         '#FFDB5C',
         '#ff9f7f',
         '#fb7293'],
-      
-  radius: ['30%', '70%'],
-      data: [
-        { value: 1048, name: 'Search Engine' },
-        { value: 735, name: 'Direct' },
-        { value: 580, name: 'Email' },
-        { value: 484, name: 'Union Ads' },
-        { value: 300, name: 'Video Ads' }
-      ],
-      label: {
-        show: false
-      },
+      radius: ['50%','10'],
+      data: selectedProps,
       emphasis: {
         itemStyle: {
           shadowBlur: 10,
           shadowOffsetX: 0,
           shadowColor: 'rgba(0, 0, 0, 0.5)'
-        },
-        query: 
-       {
-          minWidth: 200,
-          maxHeight: 30,
-          minAspectRatio: 1.3
-      }
+        }
       }
     }
   ]
-};
+      };
 
-option && myChart.setOption(option);
+
+echarts.init(document.getElementById('fifth-box')).setOption(chart3);
+
+})
+
+
+
+
+
+
+
+
